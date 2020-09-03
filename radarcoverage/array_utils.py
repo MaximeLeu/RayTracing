07e8c0bcd1a34c2e6bb4621b7de774faf93c6251
Eertmans <jeertmans@icloud.com>
2020-09-03 13:28:37 +0200

    docs

--- radarcoverage/array_utils.py
+++ radarcoverage/array_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 def sort_by_columns(array, columns=None, axis=1):
     """
     Sorts an array by columns (or any other axis specified) in a given order.
-    By default, will sort by columns, with first columns being the most decisive and the last column being the least
+    By default, will sort by columns, with first column being the most important and the last column being the least
     one. This function's behavior is similar to the one of :func:`pandas.DataFrame.sort_values`.
 
     :param array: the array to be sorted
--- radarcoverage/container_utils.py
+++ radarcoverage/container_utils.py
@@ -1,9 +1,32 @@
 class ManyToOneDict(dict):
+    """
+    Subclass of dict providing multiple keys access to identical values.
+    
+    :Example:
 
+    >>> d = ManyToOneDict()
+    >>> d[5] = 4
+    >>> d[4] = 1
+    >>> d[1] = 0
+    >>> d[4, 5,...]  # Match any key containing 4 or 5
+    [((5,), 4), ((4,), 1)]
+    >>> d[4, 5,...] = 4
+    >>> d[4, 5,...]
+    [((5,), 4), ((4,), 4)]
+    >>> d[5, 1] = 0
+    >>> d[5]  # Will always return a list of tuples
+    [((5,), 4), ((5, 1), 0)]
+    >>> d[5, 1]  # Will always return a tuple
+    ((5, 1), 0)
+    """
     def __setitem__(self, key, value):
         if not isinstance(key, tuple):
             key = (key,)
-
+        elif key[-1] == Ellipsis:
+            keys = key[:-1]
+            for key in self.keys():
+                if any(i in key for i in keys):
+                    super().__setitem__(key, value)
         super().__setitem__(key, value)
 
     def __getitem__(self, item):
@@ -11,6 +34,12 @@ class ManyToOneDict(dict):
             ret = []
             for key in self.keys():
                 if item in key:
+                    ret.append((key, super().__getitem__(key)))
+            return ret
+        elif item[-1] == Ellipsis:
+            ret = []
+            for key in self.keys():
+                if any(i in key for i in item[:-1]):
                     ret.append((key, super().__getitem__(key)))
             return ret
         else:
