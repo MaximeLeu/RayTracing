99a23281cd7e6d1a899cc5adbc376df5663f3494
MaxTour <Maxime.leurquin@gmail.com>
2022-10-17 11:44:56 +0200

    update gitignore

--- raytracing/test_ex.py
+++ raytracing/test_ex.py
@@ -15,7 +15,7 @@ from raytracing import file_utils
 
 #driver code
 if __name__ == '__main__':
-    
+    plt.close('all') #close all previous plots
     file_utils.chdir_to_file_dir(__file__)
 
     # 1. Load data
@@ -53,21 +53,24 @@ if __name__ == '__main__':
         rx2 = rx2.reshape(-1, 3)
         
         
-    elif geometry == 'dummy':
+    elif geometry == 'dummy': #TODO: doesn't work because not preprocessed
         tx = np.array([5., 12., 5.]).reshape(1, 3)
         rx = np.array([65., 12., 5.]).reshape(1, 3)
 
         square_1 = geom.Square.by_2_corner_points(np.array([[13, 22, 0], [17, 24, 0]]))
-        building_1 = geom.Building.by_polygon_and_height(square_1, 10)
+        building_1 = geom.Building.by_polygon_and_height(square_1, 10,1)
         square_2 = geom.Square.by_2_corner_points(np.array([[33, 0, 0], [37, 2, 0]]))
-        building_2 = geom.Building.by_polygon_and_height(square_2, 10)
+        building_2 = geom.Building.by_polygon_and_height(square_2, 10,2)
         square_3 = geom.Square.by_2_corner_points(np.array([[53, 22, 0], [57, 24, 0]]))
-        building_3 = geom.Building.by_polygon_and_height(square_3, 10)
+        building_3 = geom.Building.by_polygon_and_height(square_3, 10,3)
 
         ground = geom.Square.by_2_corner_points(np.array([[0, 0, 0], [70, 24, 0]]))
 
         place = geom.OrientedPlace(geom.OrientedSurface(ground), [building_1, building_2, building_3])
-
+        #dummy_filename=place.to_json(filename="../data/dummy.json")
+        #geom.preprocess_geojson(dummy_filename)
+        
+        
     elif geometry == 'my_geometry':
         geometry_filename="../data/TutorialTest/sciences.geojson"
         
@@ -95,16 +98,18 @@ if __name__ == '__main__':
     place.add_set_of_points(rx)
     #place.add_set_of_points(rx2)
     
-    ax = place.plot3d(ret=True)
-    place.center_3d_plot(ax)
-    ax=plot_utils.add_points_to_3d_ax(ax=ax, points=tx, label="TX")
+    # This plot is here to check that you geometry is correct. (TODO doesn't work)
+    #ax = place.plot3d(ret=True)
+    #place.center_3d_plot(ax)
+    #ax=plot_utils.add_points_to_3d_ax(ax=ax, points=tx, label="TX")
     #plt.legend()
-    #plt.show()       # This plot is here to check that you geometry is correct.
+    #plt.show()       
     
     #compute the rays
     problem = RayTracingProblem(tx, place)
     problem.solve(max_order=3)
     fig = plt.figure()
+    fig.set_dpi(300)
     ax = fig.add_subplot(projection='3d')
     ax = problem.plot3d(ax=ax)
     problem_path='../data/problem.json'
